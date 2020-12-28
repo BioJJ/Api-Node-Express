46dff785359be13b38b3cb94f365f160e8023a90
BioJJ <Bio.jeffcoelho@gmail.com>
2020-12-28 17:19:55 -0400

    creating user controllers, updating routes

--- app.js
+++ app.js
@@ -1,18 +1,11 @@
 const express =  require('express');
 const cors = require('cors');
 
-const user = require('./src/routes/users');
-
 const app = express();
 
 app.use(express.json({extended: true}));
 app.use(cors());
 
-// app.use('/', function(req, res) {
-//     res.send('wiki :-)');
-// });
-
-// app.use('/user', user);
 require('./src/routes/users')(app);
 require('./src/routes/projectController')(app);
 
